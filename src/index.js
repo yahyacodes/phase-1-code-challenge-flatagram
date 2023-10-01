8310b0001832f58d197f5b65e7307240d26e62f8
yahyacodes <yahya.codin@gmail.com>
2023-10-01 15:44:17 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {});
+
+// Post content displayed
+const titleDisplayed = document.querySelector("#card-title");
+const imageDisplayed = document.querySelector("#card-image");
+const likesDidplayed = document.querySelector("#like-count");
+
+// Comments content displayed
+const commentsList = document.querySelector("#comments-list");
+const heartButton = document.querySelector("#like-button");
+
+// Adding likes after click
+let likesAdded = document.querySelector(".likes");
+let likesCount = 0;
+likesAdded.textContent = likesCount;
+
+// Fetching images from db.json
+fetch("http://localhost:3000/images")
+  .then((res) => res.json())
+  .then((data) =>
+    data.forEach((posts) => {
+      imageDisplayed.src = posts.image;
+      titleDisplayed.innerHTML = posts.title;
+      likesDidplayed.innerHTML = `${posts.likes} likes`;
+    })
+  );
+
+// Fetching comments from db.json
+fetch("http://localhost:3000/comments")
+  .then((res) => res.json())
+  .then((data) =>
+    data.forEach((commentMade) => {
+      commentsList.innerHTML = commentMade.content;
+    })
+  );
+
+// Adding likes event handler
+heartButton.addEventListener("click", () => {
+  likesCount++;
+  likesAdded.innerHTML = `${likesCount} likes`;
+});
